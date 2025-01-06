c3bc6bc2c35df6456796b90dcbbca828b443b413
AlvaroFerGar <alvarofergar95@gmail.com>
2025-01-06 17:50:35 +0100

    Ejemplo de domain más complejo

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ window.onload = function() {
     // Parámetros de la simulación
     const mouseAuraRadius = 100;
     const domainRadius = 200;
-    const numberOfBalls = 1;
+    const numberOfBalls = 10;
     const ballRadius = 15;
 
     // Crear el círculo y dominio en Paper.js
@@ -19,13 +19,40 @@ window.onload = function() {
     mouseAura.strokeWidth = 0;
     mouseAura.strokeColor = 'black';
 
-    //Domain
-    const domain = new paper.Path.Rectangle({
+    // Crear un CompoundPath para el dominio
+    let domain = new paper.CompoundPath();
+
+    // Crear el rectángulo base y añadirlo al CompoundPath
+    const domainRect = new paper.Path.Rectangle({
         center: paper.view.center,
-        size: domainRadius * 2
+        size: [domainRadius * 2, domainRadius * 2]
     });
+    domain.addChild(domainRect);
+
+    // Longitud de las líneas externas
+    const segmentLength = 80;
+
+    // Centros y desplazamientos para las líneas adicionales
+    const centers = [
+        [domainRect.bounds.center.x, domainRect.bounds.topCenter.y, 0, segmentLength], // Línea hacia arriba
+        [domainRect.bounds.center.x, domainRect.bounds.bottomCenter.y, 0, -segmentLength],
+        [domainRect.bounds.leftCenter.x, domainRect.bounds.center.y, segmentLength, 0],
+        [domainRect.bounds.rightCenter.x, domainRect.bounds.center.y, -segmentLength,0]
+    ];
+
+    // Crear y añadir líneas al CompoundPath
+    centers.forEach(([x, y, dx, dy]) => {
+        const line = new paper.Path.Line({
+            from: new paper.Point(x, y),
+            to: new paper.Point(x + dx, y + dy)
+        });
+        domain.addChild(line);
+    });
+
+    // Estilo para el CompoundPath
     domain.strokeWidth = 18;
     domain.strokeColor = 'black';
+
 
 
     // Crear el motor de física
